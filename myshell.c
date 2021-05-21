e85cf8e30ba3bec78732c5d28373a9253923b4c6
browonkim <browonkim@gmail.com>
2021-05-22 07:40:32 +0900

    입력받고 기본실행까지 구현했습니다.

--- myshell.c
+++ myshell.c
@@ -15,15 +15,15 @@ enum boolean
 } typedef boolean;
 
 void getCommand(char *buffer, size_t bufferSize);
-void parseCommand(char *buffer, char list[][MAXSTRING], int *listSize);
-void executeCommand(char list[][MAXSTRING], int listSize);
-
-
+void parseCommand(char *buffer, char** list, int *listSize);
+void executeCommand(char** list, int listSize);
+void clearList(char** list, int listSize);
 int main()
 {
     char commandBuffer[BufferSize];
     const size_t commandBufSize = sizeof(commandBuffer);
-    char list[MAXSTRING][MAXSTRING];
+    char** list = (char **)malloc(sizeof(char *)*MAXSTRING);    //char * list[MAXSTRING]
+    memset(list, 0, sizeof(char*)*MAXSTRING);
     int listSize = 0;
     int i = 0;
     while (infinite)
@@ -44,7 +44,9 @@ int main()
         printf("token size = %d\n", listSize);
 #endif
         executeCommand(list, listSize);
+        clearList(list, listSize);
     }
+    free(list);
 }
 
 void getCommand(char *buffer, size_t bufferSize)
@@ -61,28 +63,39 @@ void getCommand(char *buffer, size_t bufferSize)
     }
 }
 
-void parseCommand(char *buffer, char list[][MAXSTRING], int *listSize)
+void parseCommand(char *buffer, char** list, int *listSize)
 {
     char *token = strtok(buffer, " \n");
     *listSize = 0;
     while (token != NULL)
     {
+        list[*listSize] = (char*)malloc(sizeof(char)*(strlen(token)+1));
         strcpy(list[(*listSize)], token);
         (*listSize)++;
         token = strtok(NULL, " \n");
     }
+    list[*listSize] = NULL;
 }
 
-void executeCommand(char list[][MAXSTRING], int listSize)
+void executeCommand(char** list, int listSize)
 {
     pid_t pid;
     int status;
     pid = fork();
     if(pid == 0){
         //child
+        execv(list[0],list);
+        printf("myshell: command not found: %s\n", list[0]);
+        exit(0);
     }
     else{
         //parent
         wait(&status);
     }
+}
+
+void clearList(char** list, int listSize){
+    int i = 0;
+    for(i=0;i<listSize;i++)
+        free(list[i]);
 }
